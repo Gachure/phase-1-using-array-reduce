340f0ce17aebc10172897fafed251650fd22c9c4
victor <victor.gachure@student.moringaschool>
2024-03-25 15:44:09 +0300

    done lab

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const assembledBatteries = [];
+const totalBatteries = assembledBatteries.reduce((total, current) => total + current, 31);
